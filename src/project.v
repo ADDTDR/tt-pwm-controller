868413dcbccd7923b800724544e45cde012fcc1a
Andrei <atudoroi@yahoo.com>
2026-03-15 16:15:09 +0000

    WIP

--- src/project.v
+++ src/project.v
@@ -36,10 +36,13 @@ main main(
    assign uio_oe = 8'b1110_1111;
   // All output pins must be assigned. If not used, assign to 0.
   // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  // assign uio_out = 0;
+  assign uio_out[2:0] = 0;
+  assign uio_out[5:4] = 0;
+  assign uio_out[7] = 0;
+  
   // assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, ui_in, uio_in[2:0], uio_in[7:4], rst_n, 1'b0};
+  wire _unused = &{ena, clk,  rst_n, 1'b0};
 
 endmodule
