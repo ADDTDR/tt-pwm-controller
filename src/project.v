ff07527662ef795916bed32113e62dae27f8444f
Andrei <atudoroi@yahoo.com>
2026-03-15 15:54:12 +0000

    Update connections

--- src/project.v
+++ src/project.v
@@ -32,12 +32,14 @@ main main(
 	.RX_FROM_FTDI(uio_in[3])
    
     );
+
+   assign uio_oe = 8'b1110_1111;
   // All output pins must be assigned. If not used, assign to 0.
   // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   // assign uio_out = 0;
   // assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, ui_in, uio_in[2:0], uio_in[7:4], rst_n, 1'b0};
 
 endmodule
